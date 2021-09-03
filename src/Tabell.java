72ebcfa05f7d273e6aed52fcec50c9247bdf7f84
ennyli <69962472+ennyli@users.noreply.github.com>
2021-09-03 15:40:01 +0200

    Lagde ny klasse til Tabell.java for Oppgave 1 i avsnitt 1.2.1

---
+++ src/Tabell.java
@@ -0,0 +1,31 @@
+public class Tabell {
+    private Tabell() { } // privat standardkontruktør som hindrer instansiering
+
+    /**
+     *  Oppgave 1 - avsnitt 1.2.1
+     *  Lag to min-metoder (metoder som finner posisjonen til den minste verdien) tilsvarende
+     *  de to maks-metodene i Programkode 1.2.1 b) og 1.2.1 c). Legg dem i samleklassen Tabell.
+     * @return
+     */
+
+    public static int min(int[] a, int fra, int til) {
+        if (fra < 0 || til > a.length || fra > til) {
+            throw new IllegalArgumentException("Ulovlig intervall!");
+        }
+
+        int minst = fra;
+        int minstverdi = a[fra];
+
+        for (int i = fra + 1; i < til; ++i) {
+            if (a[i] < minstverdi) {
+                minstverdi = a[i];
+                minst = i;
+            }
+        }
+        return minst; // returnerer [fra:til)
+    }
+
+    public static int min(int[] a) {
+        return min(a,0,a.length); // kaller på metoden over og returnerer hele listen.
+    }
+}
--- src/Ukeoppgaver2.java
+++ src/Ukeoppgaver2.java
@@ -1,9 +1,6 @@
 import java.util.Arrays;
-
 public class Ukeoppgaver2 {
     public static void main(String[] args) {
         // Binærtre
-
     }
-
 }
