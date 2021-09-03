152d451263ea73608461c5fcafdb35106e531342
ennyli <69962472+ennyli@users.noreply.github.com>
2021-09-03 13:34:25 +0200

    Oppdatert HelloHost2021.java: Funksjonene finner makverdi i listen Sorterer listen Printer ut før og etter liste.

--- src/HelloHost2021.java
+++ src/HelloHost2021.java
@@ -1,3 +1,54 @@
-public class HelloHost2021 {
+import java.util.Arrays;
 
+public class HelloHost2021 {
+    /**
+     * Windows: [alt] + [enter] mens du holder på klassen for å lage tester.
+     */
+
+    // forkortelse 'main' autofyller main-metoden.
+    public static void main(String[] args) {
+
+        //forkortelse 'sout' autofyller system-out-print-line.
+        System.out.println("Hei AlgDat!");
+
+        int [] values = {1,7,2,4,6,9};
+        System.out.println("Arrayet før sortering: " + Arrays.toString(values) + "\n");
+
+        myFirstSort(values);
+    }
+
+    public static int findMax(int[] values, int fra, int til) {
+        // initialiserer ved å se på første kort og sett det som foreløpig størst.
+        int index = fra;
+        int max_value = values[fra];
+
+        // forkortelse 'fori' autofyller for-loop med i som index.
+        // loopen må oppdateres for egendefinert loop.
+        // halvåpent intervall >> [begin, end) - inkluderer ikke sistnevnte verdi.
+        for (int i = fra + 1; i < til; ++i)
+            if (values[i] > max_value) {
+                max_value = values[i];
+                index = i;
+            }
+        return index;
+    }
+
+    public static void myFirstSort(int[] values) {
+        for (int k = 0; k < values.length-1; ++k) {
+
+            int max_index = findMax(values, k, values.length);
+
+            // bytter plass på tall på plass 0 og max_index
+            int temp = values[k];
+            values[k] = values[max_index];
+            values[max_index] = temp;
+        }
+
+        // printer ut arrayet etter sortering
+        System.out.print("Arrayet etter sortering: ");
+        for (int i = 0; i < values.length; ++i) {
+            System.out.print(values[i] + ", \n");
+        }
+    }
+    // lukket intervall >> [left, right] - inkluderer sistnevnte verdi.
 }
---
+++ src/Ukeoppgaver2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Ukeoppgaver2 {
+    public static void main(String[] args) {
+        // Binærtre
+
+    }
+
+}
